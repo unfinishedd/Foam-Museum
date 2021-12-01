1c75132d3eb9c8867786233385640873bc1b0c9d
Kaan Secen <24114@ma-web.nl>
2021-12-01 12:48:45 +0100

    Load posts from wordpress

--- index.php
+++ index.php
@@ -1,12 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Foam - Museum</title>
 </head>
+
 <body>
     <h1>Test Homepage</h1>
+    <?php
+
+    get_header();
+
+    if (have_posts()) :
+        while (have_posts()) : the_post(); ?>
+
+            <h2><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
+            <?php the_content() ?>
+
+    <?php endwhile;
+
+    else :
+        echo '<p>There are no posts!</p>';
+
+    endif;
+
+    get_footer();
+
+    ?>
+
+
 </body>
+
 </html>

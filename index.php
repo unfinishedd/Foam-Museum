2906ae758cf92329f7b19211d82707c94ccd443d
Kaan Secen <24114@ma-web.nl>
2021-12-09 10:12:31 +0100

    SCSS vars/database connected menu

--- index.php
+++ index.php
@@ -7,20 +7,20 @@ if (have_posts()) :
     while (have_posts()) : the_post(); ?>
 
         <!-- the body -->
-        <section class="bg-primary text-white">
-            <div class="container mx-auto text-white">
-                <?php if (has_post_thumbnail()) : ?>
-                    <figure class="w-full flex justify-end">
-                        <img class="img-small text-white" src="<?php the_post_thumbnail_url('smallest'); ?>" />
-                    </figure>
-                <?php endif ?>
-                <h2 class="underline text-white"><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
-                <?php the_content() ?>
-                <a href="<?php the_permalink() ?>">Lees Meer</a>
-                <hr>
-            </div>
-        </section>
-
+        <a href="<?php the_permalink() ?>">Lees Meer
+            <section class="bg-primary text-white">
+                <div class="container mx-auto text-white">
+                    <?php if (has_post_thumbnail()) : ?>
+                        <figure class="w-full flex justify-end">
+                            <img class="img-small text-white" src="<?php the_post_thumbnail_url('smallest'); ?>" />
+                        </figure>
+                    <?php endif ?>
+                    <h2 class="text-white"><?php the_title() ?></h2>
+                    <?php the_content() ?>
+                    <hr>
+                </div>
+            </section>
+        </a>
 
 
 
